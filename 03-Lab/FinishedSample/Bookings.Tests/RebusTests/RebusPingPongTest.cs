22d8b479f82af0abc875722fc259f0568093b664
Andrea Baducci <mtb.snowboard@gmail.com>
2015-02-09 17:44:20 +0100

    Added Rebus to Bookings.Tests

---
+++ 03-Lab/FinishedSample/Bookings.Tests/RebusTests/RebusPingPongTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Bookings.Tests.RebusTests
+{
+    [TestFixture]
+    public class RebusPingPongTest
+    {
+        [Test]
+        public void ping_pong()
+        {
+        
+        }
+    }
+}
