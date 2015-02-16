98d628220a17cb7fad169a5c9c2488a139631716
Andrea Baducci <mtb.snowboard@gmail.com>
2015-02-16 11:12:38 +0100

    folders cleanup

--- 01-intro/FinishedSample/SimpleEventStore/Query/ItemModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleEventStore.Query
-{
-    public class ItemModel
-    {
-        public string Id { get; set; }
-        public string Sku { get; set; }
-        public string Description { get; set; }
-    }
-}
